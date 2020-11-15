35ad35aa27329e33445f082f9013ad8e1af0d279
SemykinaOlga <semikinaolga@mail.ru>
2020-11-15 15:39:06 +0300

    Add good test

--- src/test/java/ru/netology/domain/ApiPostman.java
+++ src/test/java/ru/netology/domain/ApiPostman.java
@@ -21,7 +21,7 @@ public class ApiPostman {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("1234"))
+                .body("data", equalTo("123"))
         ;
     }
 }
